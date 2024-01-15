8ee66fdb44d708694ba401851472b1bde70ab42d
book3039 <book3039@knou.ac.kr>
2024-01-15 13:45:55 +0900

    (feat): create FAB

--- app/src/main/java/com/example/mywishlistapp/AppBar.kt
+++ app/src/main/java/com/example/mywishlistapp/AppBar.kt
@@ -2,10 +2,15 @@ package com.example.mywishlistapp
 
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.IconButton
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 
@@ -14,16 +19,35 @@ fun AppBarView(
     title: String,
     onBackNavClicked: () -> Unit = {}
 ) {
-    TopAppBar(title = {
-        Text(
-            text = title, color = colorResource(id = R.color.white),
-            modifier = Modifier
-                .padding(start = 4.dp)
-                .heightIn(max = 24.dp)
-        )
-    },
+    val navigationIcon: (@Composable () -> Unit)? =
+        if (!title.contains("WishList")) {
+            {
+                IconButton(onClick = { onBackNavClicked() }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        tint = Color.White,
+                        contentDescription = null
+                    )
+                }
+            }
+        } else {
+            null
+        }
+
+
+
+    TopAppBar(
+        title = {
+            Text(
+                text = title, color = colorResource(id = R.color.white),
+                modifier = Modifier
+                    .padding(start = 4.dp)
+                    .heightIn(max = 24.dp)
+            )
+        },
         elevation = 3.dp,
         backgroundColor = colorResource(id = R.color.app_bar_color),
-        )
+        navigationIcon = navigationIcon
+    )
 
 }
